4b6f6cbe81cc7ef2d2be46d7140118c299a99064
irvingdc <irvingedc@gmail.com>
2020-12-02 22:05:37 -0600

    added missing js code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+!function(t,n){function e(){if(!a){a=!0;for(var t=0;t<d.length;t++)d[t].fn.call(window,d[t].ctx);d=[]}}function o(){"complete"===document.readyState&&e()}t=t||"docReady",n=n||window;var d=[],a=!1,c=!1;n[t]=function(t,n){if("function"!=typeof t)throw new TypeError("callback for docReady(fn) must be a function");return a?void setTimeout(function(){t(n)},1):(d.push({fn:t,ctx:n}),void("complete"===document.readyState?setTimeout(e,1):c||(document.addEventListener?(document.addEventListener("DOMContentLoaded",e,!1),window.addEventListener("load",e,!1)):(document.attachEvent("onreadystatechange",o),window.attachEvent("onload",e)),c=!0)))}}("docReady",window);
+
 var yearsAnimated = false
 var yellowIconsDisplayed = false
 var servicesDisplayed = false
